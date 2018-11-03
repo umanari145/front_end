2228f6175a61747ac8caeb7360239505523a0e9b
Norio <umanari145@gmail.com>
2018-11-03 17:58:46 +0900

    不要ファイルの見直しとレスんポンシブ対応

--- gulpfile.js
+++ gulpfile.js
@@ -13,18 +13,18 @@ const browserReload = function() {
   browserSync.reload()
 }
 
-const compile_sass = function(){
+const compileSass = function(){
   console.log('sass execute')
   return gulp.src(['./phpschool/css/style2.scss'])
       .pipe(gulp_sass({outputStyle:'expanded'}))
       .pipe(gulp.dest('./phpschool/css'))
 }
-gulp.task('sass', compile_sass)
+gulp.task('sass', compileSass)
 
 
 gulp.task('watch', function() {
   gulp.watch('./phpschool/**/*', function(){
-    compile_sass()
+    compileSass()
     browserReload()
   })
 });
@@ -33,6 +33,7 @@ gulp.task('watch', function() {
 gulp.task('default', function() {
   return runSequence(
     'server',
+    'sass',
     'watch'
   );
 })
